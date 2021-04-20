8ad329b4b5e91c821f912d512b15ea6c1dd21499
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2021-04-20 09:41:32 -0300

    Create loading spinner component

---
+++ components/LoadingSpinner.vue
@@ -0,0 +1,23 @@
+<template>
+  <span class="circle"></span>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.circle {
+  animation: is-rotating 1s infinite;
+  border: 6px solid #e5e5e5;
+  border-radius: 50%;
+  border-top-color: $blue;
+  height: 50px;
+  width: 50px;
+}
+@keyframes is-rotating {
+  to {
+    transform: rotate(1turn);
+  }
+}
+</style>
