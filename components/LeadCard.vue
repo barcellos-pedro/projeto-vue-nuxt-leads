1515cc2a1e459ec270f486f865f1da69445cef94
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2021-04-20 09:03:45 -0300

    Style lead card border

--- components/LeadCard.vue
+++ components/LeadCard.vue
@@ -44,7 +44,6 @@ export default {
 .lead {
   margin: 1rem 0;
   display: grid;
-  // grid-template-columns: 10% 16% 16% 16% 16% 16%;
   grid-template-columns: 8% 13% 26% 13% 13% 16%;
   grid-gap: 0 1rem;
   text-align: center;
@@ -52,17 +51,23 @@ export default {
   padding: 8px 16px;
   border: 1px solid #fff;
   transition: all 0.4s ease;
+  &::after {
+    content: '';
+    display: block;
+    width: 90vw;
+    border-bottom: 1px solid $border-color;
+  }
   @media (max-width: 767px) {
     display: flex;
     flex-direction: column;
     align-items: center;
   }
-  &__image {
-    border-radius: 100%;
-  }
   &:hover {
     border: 1px solid $green;
     border-radius: 4px;
+    &::after {
+      display: none;
+    }
     img {
       border: 3px solid $green;
     }
@@ -79,6 +84,7 @@ p {
   width: 100%;
 }
 img {
+  border-radius: 100%;
   border: 3px solid #fff;
   padding: 2px;
   transition: border 0.4s ease;
