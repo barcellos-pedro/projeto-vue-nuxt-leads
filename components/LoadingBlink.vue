f08e79b898907f020fa56c6ddb0e876d3c965ca1
Pedro Reis <pedro.dosreis@betalabs.com.br>
2021-04-20 19:35:08 -0300

    Create loading blink component

---
+++ components/LoadingBlink.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="blink"></div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+@keyframes blinker {
+  from {
+    opacity: 1;
+  }
+  to {
+    opacity: 0;
+  }
+}
+.blink {
+  width: 25px;
+  height: 25px;
+  background-color: light($secondary, 10%);
+  border-radius: 100%;
+  display: block;
+  animation: 0.6s ease-in-out infinite alternate blinker;
+}
+</style>
